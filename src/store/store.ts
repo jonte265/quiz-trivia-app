634d279660ab85d17a5f456fde584ad29e1001da
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-04-02 22:59:54 +0200

    add random shuffle questions

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,71 +1,83 @@
 import { create } from 'zustand';
 import questions from '../data/questions.json';
 
+// Function to mix questions json
+function shuffleQuestions() {
+  return [...questions].sort(() => Math.random() - 0.5);
+}
+
 type GameStoreType = {
   score: number;
   currentQuestionIndex: number;
   currentQuestion: string;
   currentOptions: string[];
   correctAnswer: string;
-
   restartMode: boolean;
   gameCompleteMode: boolean;
   nextAnswer: (answer: string) => void;
   restartGame: () => void;
 };
 
-const useGameStore = create<GameStoreType>((set) => ({
-  score: 0,
-  currentQuestionIndex: 0,
+const useGameStore = create<GameStoreType>((set) => {
+  const shuffledQuestions = shuffleQuestions();
 
-  currentQuestion: questions[0].question,
-  currentOptions: questions[0].options,
-  correctAnswer: questions[0].answer,
+  return {
+    score: 0,
+    currentQuestionIndex: 0,
 
-  restartMode: false,
-  gameCompleteMode: false,
+    currentQuestion: shuffledQuestions[0].question,
+    currentOptions: shuffledQuestions[0].options,
+    correctAnswer: shuffledQuestions[0].answer,
 
-  nextAnswer: (answer) =>
-    set((state) => {
-      // Check correct answer
-      const isCorrect = answer === state.correctAnswer;
-      const nextIndex = state.currentQuestionIndex + 1;
+    restartMode: false,
+    gameCompleteMode: false,
 
-      if (isCorrect) {
-        // Check if no more questions
-        if (state.currentQuestionIndex >= questions.length - 1) {
+    nextAnswer: (answer) =>
+      set((state) => {
+        // Check correct answer
+        const isCorrect = answer === state.correctAnswer;
+        const nextIndex = state.currentQuestionIndex + 1;
+
+        if (isCorrect) {
+          // Check if no more questions
+          if (nextIndex >= shuffledQuestions.length) {
+            return {
+              score: state.score + 1,
+              currentQuestion: 'Game Complete! 🎉',
+              gameCompleteMode: true,
+            };
+          }
+
           return {
+            currentQuestionIndex: nextIndex,
             score: state.score + 1,
-            currentQuestion: 'Game Complete! 🎉',
-            gameCompleteMode: true,
+            currentQuestion: shuffledQuestions[nextIndex].question,
+            currentOptions: shuffledQuestions[nextIndex].options,
+            correctAnswer: shuffledQuestions[nextIndex].answer,
+          };
+        } else {
+          return {
+            currentQuestion: 'Wrong, Game over',
+            restartMode: true,
           };
         }
+      }),
+
+    restartGame: () =>
+      set(() => {
+        const newShuffledQuestions = shuffleQuestions();
 
         return {
-          currentQuestionIndex: nextIndex,
-          score: state.score + 1,
-          currentQuestion: questions[nextIndex].question,
-          currentOptions: questions[nextIndex].options,
-          correctAnswer: questions[nextIndex].answer,
+          score: 0,
+          currentQuestionIndex: 0,
+          currentQuestion: newShuffledQuestions[0].question,
+          currentOptions: newShuffledQuestions[0].options,
+          correctAnswer: newShuffledQuestions[0].answer,
+          restartMode: false,
+          gameCompleteMode: false,
         };
-      } else {
-        return {
-          currentQuestion: 'Wrong, Game over',
-          restartMode: true,
-        };
-      }
-    }),
-
-  restartGame: () =>
-    set(() => ({
-      score: 0,
-      currentQuestionIndex: 0,
-      currentQuestion: questions[0].question,
-      currentOptions: questions[0].options,
-      correctAnswer: questions[0].answer,
-      restartMode: false,
-      gameCompleteMode: false,
-    })),
-}));
+      }),
+  };
+});
 
 export default useGameStore;
